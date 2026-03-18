d9bbf14398aaf967f03d0e8d9487314a4a265cad
alari409 <f1120726@gmail.com>
2026-03-18 14:47:07 +0900

    thks

---
+++ Week2/2-5square.cpp
@@ -0,0 +1,51 @@
+﻿//2-5square.cpp
+#include <iostream>
+
+//표준 라이브러리에서 가져와서 사용할 이름을 언급
+using std::cin;
+using std::cout;
+using std::endl;
+
+// *네모 만들기
+int main(void)
+{
+	cout << "1. Square OR 2. Rectangle";
+
+	int option;
+	cin >> option;
+
+	if (option != 1 && option != 2)
+	{
+		cout << "Error: only choose 1 or 2";
+		return 0x712F;
+	}
+
+	cout << "What size?";
+	int size;
+	cin >> size;
+
+	//행 출력
+	for (int i = 0; i < size; i++)
+	{//열 출력
+		for (int j = 0; j < size; j++)
+		{
+			if (option == 1) //square
+			{
+				cout << "*";
+			}
+			else if (option == 2) //rect
+			{
+				cout << "**";
+			}
+			else
+			{
+				cout << "Error: only choose 1 or 2.";
+				return 0x34AF;
+			}
+		}
+		// 행 끝에 줄 바꿈
+		cout << endl;
+	}
+
+	return 0;
+}
---
+++ Week2/triangle.cpp
@@ -0,0 +1,36 @@
+﻿//triangle.cpp
+#include <iostream>
+
+//표준 라이브러리에서 가져와서 사용할 이름을 언급
+using std::cin;
+using std::cout;
+using std::endl;
+
+// *네모 만들기
+int main(void)
+{
+	cout << "what size of triangle?";
+	
+	int size;
+	cin >> size;
+
+	
+
+
+	//행 출력
+	for (int i = 0; i < size; i++)
+	{//열 출력
+		for (int j = 0; j < i; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < (2 * size) - 1 - (i * 2); j++)
+		{
+			cout << "*";
+		}
+		// 행 끝에 줄 바꿈
+		cout << endl;
+	}
+
+	return 0;
+}
